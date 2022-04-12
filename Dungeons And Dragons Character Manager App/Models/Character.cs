761d28d6ec12d43299c20c0dc2fea0bd3d03fec4
Conrad <conrad@bbd.co.za>
2022-04-12 19:46:24 +0200

    Added Character model

---
+++ Dungeons And Dragons Character Manager App/Models/Character.cs
@@ -0,0 +1,32 @@
+namespace Dungeons_And_Dragons_Character_Manager_App.Models
+{
+    public class Character 
+    {
+        public int ID {get; set;}
+        public string name {get; set;}
+        public string playerName {get; set;}
+        public Race Race {get; set;}
+        public DndClass characterClass {get; set;}
+        public ICollection<AbilityScore> abilityScores {get; set;}
+        public Background background {get; set;}
+        public int currentHitPoints {get; set;}
+        public int temporaryHitPoints {get; set;}
+        public int maxHitPoints {get; set;}
+        public ICollection<Item> equipment {get; set;}
+        public int proficiencyBonus {get; set;} 
+        public int armourClass {get; set;} 
+        public ICollection<Skills> skills {get; set;} 
+        public ICollection<Item> itemProficiencies {get; set;} 
+        public ICollection<Skills> skillProficiencies {get; set;} 
+        public ICollection<AbilityScore> savingProficiencies {get; set;} 
+        public int experiencePoints {get; set;} 
+        public int currentHitDice {get; set;} 
+        public int totalHitDice {get; set;} 
+        public ICollection<Language> languages {get; set;} 
+        public int numGold {get; set;} 
+        public int numSilver {get; set;} 
+        public int numCopper {get; set;} 
+        public int numPlatinum {get; set;} 
+        public int numElectrum {get; set;} 
+    }
+}
