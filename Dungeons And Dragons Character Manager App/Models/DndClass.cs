863196c58c8cde146d19ebad4c0876b65e51e9b2
Conrad <conrad@bbd.co.za>
2022-04-12 16:23:50 +0200

    cleaned up some stuff from a test program

--- Dungeons And Dragons Character Manager App/Models/DndClass.cs
+++ Dungeons And Dragons Character Manager App/Models/DndClass.cs
@@ -1,20 +1,3 @@
-// A Hello World! program in C#.
-using System;
-// namespace HelloWorld
-// {
-//     class Hello 
-//     {
-//         static void Main() 
-//         {
-//             Console.WriteLine("Hello World!");
-
-//             // Keep the console window open in debug mode.
-//             Console.WriteLine("Press any key to exit.");
-//             Console.ReadKey();
-//         }
-//     }
-// }
-
 namespace Dungeons_And_Dragons_Character_Manager_App.Models
 {
     public abstract class DndClass
