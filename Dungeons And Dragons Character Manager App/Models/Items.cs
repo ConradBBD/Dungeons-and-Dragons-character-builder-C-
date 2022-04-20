f6762a9cd8e9d6d52cb5bc79c716dd425dc39d7c
Conrad <conrad@bbd.co.za>
2022-04-20 21:00:32 +0200

    Reverted Items

--- Dungeons And Dragons Character Manager App/Models/Items.cs
+++ Dungeons And Dragons Character Manager App/Models/Items.cs
@@ -1,16 +1,8 @@
 namespace Dungeons_And_Dragons_Character_Manager_App.Models;
 
-public class Item{
+public abstract class Item{
     public int Id { get; set; }
     public string Name { get; set; }
     public uint LbWeight { get; set; }
     public uint GPCost { get; set; }
-
-    public Item(
-        string Name
-        )
-    {
-        this.Name = Name;
-
-    }
 }
