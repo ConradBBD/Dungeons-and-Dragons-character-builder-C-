d47eb779ac8c588bcd908d334a8dcb3c838bf6c2
Conrad <conrad@bbd.co.za>
2022-04-20 20:54:52 +0200

    Fixed DndClass to what is was previously

--- Dungeons And Dragons Character Manager App/Models/Items.cs
+++ Dungeons And Dragons Character Manager App/Models/Items.cs
@@ -1,8 +1,16 @@
 namespace Dungeons_And_Dragons_Character_Manager_App.Models;
 
-public abstract class Item{
+public class Item{
     public int Id { get; set; }
     public string Name { get; set; }
     public uint LbWeight { get; set; }
     public uint GPCost { get; set; }
+
+    public Item(
+        string Name
+        )
+    {
+        this.Name = Name;
+
+    }
 }
