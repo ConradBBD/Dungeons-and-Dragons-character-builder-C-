b8c7728b78dd3ee1cb55dba0df807e64e8baf7d7
aris-bbd <arisn@bbd.co.za>
2022-04-20 17:11:36 +0200

    Merge conflicts character class

--- Dungeons And Dragons Character Manager App/Models/Character.cs
+++ Dungeons And Dragons Character Manager App/Models/Character.cs
@@ -5,20 +5,20 @@
         public int ID {get; set;}
         public string name {get; set;}
         public string playerName {get; set;}
-        public Race Race {get; set;}
-        public DndClass characterClass {get; set;}
-        public ICollection<AbilityScore> abilityScores {get; set;}
-        public Background background {get; set;}
+        public Race? Race {get; set;}
+        public DndClass? characterClass {get; set;}
+        public ICollection<AbilityScore>? abilityScores {get; set;}
+        //public Background background {get; set;}
         public int currentHitPoints {get; set;}
         public int temporaryHitPoints {get; set;}
         public int maxHitPoints {get; set;}
         public ICollection<Item> equipment {get; set;}
         public int proficiencyBonus {get; set;} 
         public int armourClass {get; set;} 
-        public ICollection<Skills> skills {get; set;} 
-        public ICollection<Item> itemProficiencies {get; set;} 
-        public ICollection<Skills> skillProficiencies {get; set;} 
-        public ICollection<AbilityScore> savingProficiencies {get; set;} 
+        //public ICollection<Skills> skills {get; set;} 
+        //public ICollection<Item> itemProficiencies {get; set;} 
+        public ICollection<Skill> skillProficiencies {get; set;} 
+        public ICollection<Ability> savingProficiencies {get; set;} 
         public int experiencePoints {get; set;} 
         public int currentHitDice {get; set;} 
         public int totalHitDice {get; set;} 
